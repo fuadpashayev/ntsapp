c47155c6f459744b4be924086aed6937e6330f69
Fuad Pashayev <fuad.pashayev12@gmail.com>
2018-09-08 14:11:49 +0400

    last commit

--- app/src/main/java/azweb/ntsapp/MainActivity.kt
+++ app/src/main/java/azweb/ntsapp/MainActivity.kt
@@ -7,13 +7,33 @@ import android.widget.ListAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val header = ArrayList<String>()
+    val body = ArrayList<ArrayList<String>>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val list = arrayListOf("a","a1","a2","b","b1","b2")
-        //dataList.adapter = ListAdapter(list)
+        val season1 = ArrayList<String>()
+        season1.add("Winter")
+        season1.add("Spring")
+        season1.add("Summer")
+        season1.add("Autumn")
+
+        val season2 = ArrayList<String>()
+        season2.add("The")
+        season2.add("Life")
+        season2.add("Is")
+        season2.add("Very")
+        season2.add("Beautiful")
+
+        header.add("Seasons")
+        header.add("Life information")
+
+        body.add(season1)
+        body.add(season2)
+
+        dataList.setAdapter(listAdapter(this,header,body))
 
     }
 }
--- app/src/main/java/azweb/ntsapp/listAdapter.kt
+++ app/src/main/java/azweb/ntsapp/listAdapter.kt
@@ -8,58 +8,59 @@ import android.view.ViewGroup
 import android.widget.BaseExpandableListAdapter
 import android.widget.TextView
 
-class ListAdapter(context:Context, listDataHeader:List<String>,listChildData:HashMap<String, List<String>>):BaseExpandableListAdapter() {
-    private val _context:Context
-    private val _listDataHeader:List<String> // header titles
-    // child data in format of header title, child title
-    private val _listDataChild:HashMap<String, List<String>>
-    init{
-        this._context = context
-        this._listDataHeader = listDataHeader
-        this._listDataChild = listChildData
-    }
-    override fun getGroupCount():Int {
-            return this._listDataHeader.size
+class listAdapter(var context:Context,var header:ArrayList<String>,var body:ArrayList<ArrayList<String>> ):BaseExpandableListAdapter() {
+    override fun getGroup(groupPosition: Int): String {
+        return header[groupPosition]
     }
 
-    override fun getChild(groupPosition:Int, childPosititon:Int):Any {
-        return this._listDataChild.get(this._listDataHeader.get(groupPosition))!!.get(childPosititon)
-    }
-    override fun getChildId(groupPosition:Int, childPosition:Int):Long {
-        return childPosition.toLong()
-    }
-    override fun getChildView(groupPosition:Int, childPosition:Int, isLastChild:Boolean, convertView:View, parent:ViewGroup):View {
-        val childText = getChild(groupPosition, childPosition) as String
-        val infalInflater = this._context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val ConvertView = infalInflater.inflate(R.layout.data_item, null)
-        val txtListChild = convertView.findViewById(R.id.dataList) as TextView
-        txtListChild.text = childText
-        return ConvertView
-    }
-    override fun getChildrenCount(groupPosition:Int):Int {
-        return this._listDataChild.get(this._listDataHeader.get(groupPosition))!!.size
-    }
-    override fun getGroup(groupPosition:Int):Any {
-        return this._listDataHeader.get(groupPosition)
-    }
-    override fun getGroupId(groupPosition:Int):Long {
-        return groupPosition.toLong()
-    }
-    override fun getGroupView(groupPosition:Int, isExpanded:Boolean,
-                     convertView:View, parent:ViewGroup):View {
-        val headerTitle = getGroup(groupPosition) as String
-        val infalInflater = this._context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val convertView = infalInflater.inflate(R.layout.data_item, null)
-        val lblListHeader = convertView
-                .findViewById(R.id.dataList) as TextView
-        lblListHeader.setTypeface(null, Typeface.BOLD)
-        lblListHeader.setText(headerTitle)
-        return convertView
-    }
-    override fun hasStableIds():Boolean {
-        return false
-    }
-    override fun isChildSelectable(groupPosition:Int, childPosition:Int):Boolean {
+    override fun isChildSelectable(groupPosition: Int, childPosition: Int): Boolean {
         return true
     }
+
+    override fun hasStableIds(): Boolean {
+        return false
+    }
+
+    override fun getGroupView(groupPosition: Int, isExpanded: Boolean, convertView: View?, parent: ViewGroup?): View? {
+        var convertView = convertView
+        if(convertView==null){
+            val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            convertView = inflater.inflate(R.layout.layout_group,null)
+        }
+        val title = convertView?.findViewById<TextView>(R.id.tv_title)
+        title?.text = getGroup(groupPosition)
+        return convertView
+    }
+
+    override fun getChildrenCount(groupPosition: Int): Int {
+        return body[groupPosition].size
+    }
+
+    override fun getChild(groupPosition: Int, childPosition: Int): String {
+        return body[groupPosition][childPosition]
+    }
+
+    override fun getGroupId(groupPosition: Int): Long {
+        return groupPosition.toLong()
+    }
+
+    override fun getChildView(groupPosition: Int, childPosition: Int, isLastChild: Boolean, convertView: View?, parent: ViewGroup?): View? {
+        var convertView = convertView
+        if(convertView==null){
+            val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            convertView = inflater.inflate(R.layout.layout_child,null)
+        }
+        val title = convertView?.findViewById<TextView>(R.id.tv_title)
+        title?.text = getChild(groupPosition,childPosition)
+        return convertView
+    }
+
+    override fun getChildId(groupPosition: Int, childPosition: Int): Long {
+        return childPosition.toLong()
+    }
+
+    override fun getGroupCount(): Int {
+        return header.size
+    }
+
 }
